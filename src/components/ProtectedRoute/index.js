9b1f3f8e7c6dc7625cac73474150bdd0c0f8d4de
surendra4c4 <surendra4c4@gmail.com>
2022-02-05 18:15:19 +0000

    not found route tests

---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import Cookies from 'js-cookie'
+import {Redirect, Route} from 'react-router-dom'
+
+const ProtectedRoute = props => {
+  const token = Cookies.get('jwt_token')
+
+  if (token === undefined) {
+    return <Redirect to="/login" />
+  }
+  return <Route {...props} />
+}
+
+export default ProtectedRoute
